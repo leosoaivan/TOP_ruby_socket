8e4483a0c5c96c04f7dc04d6d737ea0b6fdfee42
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-09-12 21:22:52 -0400

    Add simple_browser.rb

---
+++ simple_browser.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+host = 'localhost'     # The web server
+port = 2000                           # Default HTTP port
+path = "/index.html"                 # The file we want
+
+# This is the HTTP request we send to fetch a file
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open(host,port)  # Connect to server
+socket.print(request)               # Send request
+response = socket.read              # Read complete response
+
+# Split response at first blank line into headers and body
+puts response                       # And display it
--- simple_server.rb
+++ simple_server.rb
@@ -3,7 +3,15 @@ require 'socket'               # Get sockets from stdlib
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
-  client.puts "Closing the connection. Bye!"
+  verb,request,http_standard = client.gets.split(" ", 3)
+  request.gsub!(/\//, "")
+
+  response = File.open("#{request}", 'r').each { |line| puts line }
+
+  client.puts   "HTTP/1.0 200 OK\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: #{response.size}\r\n\r\n" +
+
+  IO.copy_stream(response, client)
   client.close                 # Disconnect from the client
 }
